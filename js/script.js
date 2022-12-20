c9b246ad2be62b65adefa0578b286b8685a76aa2
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-20 08:55:24 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -1,12 +1,58 @@
-const targetElement = document.getElementById('target');
-const btn = document.getElementById('button');
+// # FASE PREPARATORIA ---------------------------------
 
-btn.addEventListener('click', function(){
-    
-    let i = 0;
+// Recupero gli elementi in pagina
+const grid = document.getElementById('grid');
+const playButton = document.getElementById('play-button');
 
-    for (i = 1; i <= 100; i++) {
-        targetElement.innerText += i;
-    }
+function play() {
 
-})
+  // # Funzioni interne al gioco
+  const createCell = (number) => {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.append(number);
+
+    return cell;
+  }
+
+  
+
+  // Cambiamo il testo del bottone in "Ricomincia"
+  playButton.innerText = 'Ricomincia';
+
+  // Svuotiamo la griglia
+  grid.innerHTML = '';
+
+  
+
+  // ! -------------------------------
+  // ! SVOLGIMENTO EFFETTIVO
+  // ! -------------------------------
+
+  for (let j = 1; j <= 100; j++) {
+
+    const cell = createCell(j);
+
+    cell.addEventListener('click', function () {
+      cell.classList.add('clicked');
+      // console.log(i);
+    })
+
+  
+
+    grid.appendChild(cell);
+  }
+}
+
+
+
+}
+
+
+
+playButton.addEventListener('click', play);
+
+
+
+
+
